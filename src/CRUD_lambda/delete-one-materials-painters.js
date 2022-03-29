adbda29b09f8b70a2a946055ea6628878029f2f3
Muriel Gonzalez <78143766+MurielGN@users.noreply.github.com>
2022-03-29 13:48:08 +0200

    Create delete-one-materials-painters.js

---
+++ src/CRUD_lambda/delete-one-materials-painters.js
@@ -0,0 +1,51 @@
+const AWS = require("aws-sdk");
+
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+const TABLE_DYNAMODB = "materials";
+
+var statusCode;
+
+
+function verify(value) {
+  if (value <= 0 || isNaN(value)) {
+    statusCode = 400;
+    throw new Error("The id " + value + " is invalid");
+  }
+  return value;
+}
+
+exports.handler = async (event, context) => {
+  let body;
+  let statusCode = 200;
+  
+  
+  try {
+
+    body = await dynamo.delete({ 
+      TableName: TABLE_DYNAMODB,
+      ReturnValues: "ALL_OLD",
+      Key: {
+        element: 'material',
+        id: verify(event.id)
+      }
+    }).promise();
+    
+    if(body.Attributes == undefined){
+      statusCode = 404;
+      throw new Error("Material with id: " + event.id + " not found");
+    }
+    
+      return {
+      statusCode,
+      body
+    };
+  
+  } catch (err) {
+    return {
+      statusCode,
+      Error: err.toString(),
+    };
+  }
+
+}
