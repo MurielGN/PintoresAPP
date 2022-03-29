fda7093ad459acfb0c64c7894e241617c4bb5abe
Muriel Gonzalez <78143766+MurielGN@users.noreply.github.com>
2022-03-29 17:25:17 +0200

    Update get-materials-StarWithName-painters.js

--- src/CRUD_lambda/get-materials-StarWithName-painters.js
+++ src/CRUD_lambda/get-materials-StarWithName-painters.js
@@ -4,6 +4,8 @@ const DYNAMO = new AWS.DynamoDB.DocumentClient();
 
 const TABLE_DYNAMODB = "materials";
 
+var statusCode;
+
 
 async function queryItems(filterStarWith){
   var params = {
@@ -16,7 +18,13 @@ async function queryItems(filterStarWith){
   }
   
   try {
-    const data = await DYNAMO.query(params).promise()
+    const data = await DYNAMO.query(params).promise();
+    
+    if(data.Count == 0){
+      statusCode = 404;
+      throw new Error("Materials name starting with: " + filterStarWith + " not found");
+    }
+    
     return data.Items
   } catch (err) {
     return err
@@ -27,14 +35,28 @@ async function queryItems(filterStarWith){
 
 exports.handler = async (event, context) => {
   let body;
-  let statusCode = 200;
+  statusCode = 200;
 
-  let filterStarWith = event.expression;
-  body = await queryItems(filterStarWith);
+  try {
+    
+    let filterStarWith = event.expression;
+    if(typeof filterStarWith !== "string" || filterStarWith === ""){
+      statusCode = 400;
+      throw new Error("Expression: " + filterStarWith + " is invalid");
+    }
+    
+    body = await queryItems(filterStarWith);
+    
+      return {
+      statusCode,
+      body
+    };
+    
+  } catch (err) {
+      return {
+        statusCode,
+        Error: err.toString(),
+    };
+  }
   
-    return {
-    statusCode,
-    body,
-  };
-
 }
