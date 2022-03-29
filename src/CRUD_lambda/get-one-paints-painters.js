89d75d3c6261a008076c376a6217362b5e03566c
Raul Hernandez <75371880+Raul-HDEZ@users.noreply.github.com>
2022-03-29 12:42:11 +0200

    Create get-one-paints-painters.js

---
+++ src/CRUD_lambda/get-one-paints-painters.js
@@ -0,0 +1,62 @@
+const AWS = require("aws-sdk");
+
+const DYNAMO = new AWS.DynamoDB.DocumentClient();
+
+const TABLE_DYNAMODB = "paints";
+
+var statusCode;
+
+async function queryItems(id) {
+  var params = {
+    TableName: TABLE_DYNAMODB,
+    KeyConditionExpression:
+      "#partitionKeyName = :partitionkeyval AND #sortKeyName = :sortkeyval",
+    ExpressionAttributeValues: {
+      ":partitionkeyval": "paint",
+      ":sortkeyval": id,
+    },
+    ExpressionAttributeNames: {
+      "#partitionKeyName": "element",
+      "#sortKeyName": "id",
+    },
+  };
+
+  let data = await DYNAMO.query(params).promise();
+
+  if (data.Count == 0) {
+    statusCode = 404;
+    throw new Error("Material with id: " + id + " not found");
+  }
+
+  data = data.Items[0];
+  return data;
+}
+
+function verify(value) {
+  if (value <= 0 || isNaN(value)) {
+    statusCode = 400;
+    throw new Error("The id " + value + " is invalid");
+  }
+}
+
+exports.handler = async (event, context) => {
+  var body;
+  statusCode = 200;
+
+  try {
+    let id = event.id;
+    verify(id);
+
+    body = await queryItems(id);
+
+    return {
+      statusCode,
+      body,
+    };
+  } catch (err) {
+    return {
+      statusCode,
+      Error: err.toString(),
+    };
+  }
+};
